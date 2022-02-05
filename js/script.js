390bdd5546084f10f670f7794a1c63f2f16a04a8
Sina <bekarsina@gmail.com>
2022-02-05 10:14:01 +0330

    fix bug in javascript code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 var $ = document;
 
-var pass = $.querySelector(".Pass")
-var generate = $.querySelector(".generate")
+var pass = $.querySelector(".Pass");
+var generate = $.querySelector(".generate");
 
-function capchaPass(){
-  var RandomChars = "qwertyuiopasdfghjklzxcvbnm123456789!@#$%^&*()_+=-/{}[]></\|"
+function capchaPass() {
+  var RandomChars =
+    "qwertyuiopasdfghjklzxcvbnm123456789!@#$%^&*()_+=-/{}[]></|";
   var RandomText = "";
   for (let i = 0; i < 8; i++) {
-    RandomText += RandomChars.charAt(Math.floor(Math.random() * 60))
+    RandomText += RandomChars.charAt(Math.floor(Math.random() * 60));
   }
-  pass.innerHTML = RandomText;
+  if (RandomText.length == 8) {
+    pass.innerHTML = RandomText;
+  } else {
+    return Error
+  }
+  
+  console.log(RandomText.length);
+  // pass.innerHTML = RandomText;
 }
