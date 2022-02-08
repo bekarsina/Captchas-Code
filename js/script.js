1d8f67f924212055cc38c4fc2b0a2dd9b506276f
Sina <bekarsina@gmail.com>
2022-02-08 11:04:19 +0330

    New Updata

--- js/script.js
+++ js/script.js
@@ -2,20 +2,21 @@ var $ = document;
 
 var pass = $.querySelector(".Pass");
 var generate = $.querySelector(".generate");
+var passLength = $.querySelector(".pass-length");
+
 
 function capchaPass() {
+  var length = Number(passLength.value);
   var RandomChars =
     "qwertyuiopasdfghjklzxcvbnm123456789!@#$%^&*()_+=-/{}[]></|";
   var RandomText = "";
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < length; i++) {
     RandomText += RandomChars.charAt(Math.floor(Math.random() * 60));
   }
-  if (RandomText.length == 8) {
+  if (RandomText.length == length) {
     pass.innerHTML = RandomText;
   } else {
-    return Error
+    return Error;
   }
-  
-  console.log(RandomText.length);
-  // pass.innerHTML = RandomText;
+
 }
